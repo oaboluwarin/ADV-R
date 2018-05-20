33f5574bb46ae236426825166e23ee8f936e8dd9
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-20 13:58:32 +0100

    refactor reference to the app entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
   entry: [
     'babel-polyfill',
-    './lib/components/index.js'
+    './lib/renderers/dom.js'
   ],
   output: {
     path: path.resolve(__dirname, 'public'),
